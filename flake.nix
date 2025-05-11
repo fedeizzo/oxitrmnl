0ca95e45ed824d931864343c6d7f55e7ad74684a
Federico Izzo <federico@fedeizzo.dev>
2025-05-11 08:50:36 +0200

    feat: first commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Rust webserver implementation for trmnl.";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.rust-overlay.overlays.default
+          ];
+        };
+        packages.default = pkgs.hello;
+        devShells.default = with pkgs; mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-analyzer" "rust-src" "rustfmt" "clippy" ];
+            })
+          ];
+        };
+      };
+    };
+}
